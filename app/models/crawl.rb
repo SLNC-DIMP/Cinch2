3dcf586025a7cea9deff8656ecad4ffbe18a3f0f
Dean Farrell <farrelldlfarrell@gmail.com>
2012-08-08 14:59:32 -0400

    Crawl form should work correctly.

--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -4,6 +4,6 @@ class Crawl < ActiveRecord::Base
   attr_accessible :url, :pdfa, :jpeg2000, :processed, :user_id
 
   validates_presence_of :url, :on => :create
-  validates_format_of :url, :with => /\^http(s):\/\//i, :on => :create,
-                      :message => "Please add http(s):// to the front of your url"
+  validates_format_of :url, :with => URI::regexp(%w(http https)), :on => :create,
+                      :message => " - Please enter a valid url starting with http(s)://"
 end
