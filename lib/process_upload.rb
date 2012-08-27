3ce76cbc334d87db7b332fb632eb834cbc7e6bcb
Dean Farrell <farrelldlfarrell@gmail.com>
2012-08-27 11:57:26 -0400

    cleaned up ProcessUpload module so that it should work now.

--- lib/process_upload.rb
+++ lib/process_upload.rb
@@ -1,26 +1,37 @@
-require "uri"
-
 module ProcessUpload
-  # Extract links from the file as an array deleting empty lines
+  # Extract links from the file
   def read_file(file_path)
+    lines = []
     if File.exists?(file_path)
-      lines = File.readlines(file_path)
-      lines.reject! { |l| l.empty? }
+      fh = File.open(file_path)
+      fh.each_line { |l|
+        lines << l unless line_empty(l)
+      }
+      fh.close
+      lines
     end
   end
 
   # Count the number of links in the file
   def count_links(file_path)
-    read_file(file_path).count
+    read_file(file_path).length
   end
 
   # Make sure a link is a valid url
+  # regex from http://msdn.microsoft.com/en-us/library/ff650303.aspx
   def valid_link?(link)
-    begin
-      URI.parse(link)
+    regex = /^http(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/
+    if regex.match(link)
       true
-    rescue InvalidURIErrror
+    else
       false
     end
+  end
+
+  # Strip empty lines out of list
+  # TODO rails blank? may work here too.
+  # http://stackoverflow.com/questions/7339292/ruby-remove-empty-lines-from-string?lq=1
+  def line_empty(line)
+    /^$\n/.match(line)
   end
 end
